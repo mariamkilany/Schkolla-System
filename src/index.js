40f5ea2e52ab1445b6a7e9ede5ffd20ff4d2745a
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2023-07-04 20:32:53 +0300

    remove credentials

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./components/shared/AuthContext";
 // import ThemeProvider from '@mui/material/styles'
 axios.defaults.baseURL = "https://joker.animeraa.com/";
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
