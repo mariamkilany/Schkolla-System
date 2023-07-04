a01b2e96b6af1a81dd4f5b9e16a65056efbd6440
mariamkilany <70236043+mariamkilany@users.noreply.github.com>
2023-07-04 18:03:25 +0300

    change baseurl

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle'
-import App from './App';
-import axios from 'axios'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthContextProvider } from './components/shared/AuthContext';
-  // import ThemeProvider from '@mui/material/styles'
-axios.defaults.baseURL='http://localhost:4000/v1/';
-axios.defaults.withCredentials=true;
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import App from "./App";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./components/shared/AuthContext";
+// import ThemeProvider from '@mui/material/styles'
+axios.defaults.baseURL = "https://joker.animeraa.com/";
+axios.defaults.withCredentials = true;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthContextProvider>
+      <AuthContextProvider>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
-    </AuthContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
